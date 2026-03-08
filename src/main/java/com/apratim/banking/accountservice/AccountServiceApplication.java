a2ddb8f7020552452a86881def5a29a2084590de
Apratim Das <apratimdas18@gmail.com>
2026-03-08 17:08:45 +0530

    feat: Add code refactoring and use props to store static values

--- src/main/java/com/apratim/banking/accountservice/AccountServiceApplication.java
+++ src/main/java/com/apratim/banking/accountservice/AccountServiceApplication.java
@@ -1,9 +1,14 @@
 package com.apratim.banking.accountservice;
 
+import com.apratim.banking.accountservice.config.JwtConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(
+		JwtConfig.class
+)
 public class AccountServiceApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/apratim/banking/accountservice/security/JwtUtil.java
@@ -0,0 +1,43 @@
+package com.apratim.banking.accountservice.security;
+
+import com.apratim.banking.accountservice.config.JwtConfig;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+
+@Component
+public class JwtUtil {
+
+    private final JwtConfig jwtConfig;
+    public final Key key;
+
+    public JwtUtil(JwtConfig jwtConfig) {
+        this.jwtConfig = jwtConfig;
+        this.key = Keys.hmacShaKeyFor(jwtConfig.getSecret().getBytes());
+
+    }
+
+    public String extractUserName(String token){
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+    public boolean validateToken(String token){
+        try{
+            Jwts.parserBuilder()
+                    .setSigningKey(key)
+                    .build()
+                    .parseClaimsJws(token);
+
+            return true;
+        } catch (Exception e){
+            return false;
+        }
+    }
+}
