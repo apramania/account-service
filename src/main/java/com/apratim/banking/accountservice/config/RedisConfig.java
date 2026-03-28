8a22e18edbbbf06d58fac2348a065f1740691d75
Apratim Das <apratimdas18@gmail.com>
2026-03-28 13:22:46 +0530

    dockerize the application

--- src/main/java/com/apratim/banking/accountservice/config/RedisConfig.java
+++ src/main/java/com/apratim/banking/accountservice/config/RedisConfig.java
@@ -3,11 +3,18 @@ package com.apratim.banking.accountservice.config;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RedisConfig {
+
+    @Value("${REDIS_HOST}")
+    private String redisHost;
+
+    @Value("${REDIS_PORT}")
+    private String redisPort;
 
     @Bean
     public RedissonClient redissonClient() {
@@ -15,8 +22,8 @@ public class RedisConfig {
         Config config = new Config();
 
         config.useSingleServer()
-              .setAddress("redis://localhost:6379"); // Replace with your Redis server address
-        // You can customize the configuration as needed (e.g., host, port, password)
+              .setAddress("redis://" + redisHost + ":" + redisPort); // Replace with your Redis server address
+        // You can customize the configuration as needed(e.g., host, port, password)
         return Redisson.create(config);
     }
 }
